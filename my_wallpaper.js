ee4111bb40912695ef7acbca57a6abe7b691b6f3
Josh-Clarke9 <clarkejosh2@myvuw.ac.nz>
2025-07-22 11:38:54 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,6 +2,10 @@
 let rect_width  = 20;
 let rect_height = 30;
 
+let midX = 100;
+let midY = 100;
+let size = 40;
+let wing = 60;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -17,9 +21,49 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(6, 2, 112); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  strokeWeight(0);
+fill(93,226,231);//turquoise
+ellipse(midX+0,midY+0,200,200);
+
+strokeWeight(0.5);
+stroke(204,108,231);//pink
+fill(6,2,112);//purple
+ellipse(midX-25,midY-20,wing,wing-10);//top left wing
+ellipse(midX-25,midY+10,wing,wing-10);//top right wing
+ellipse(midX+25,midY-20,wing,wing-10);//bottom left wing
+ellipse(midX+25,midY+10,wing,wing-10);//bottom right wing
+
+//innerwing
+fill(93,226,231);//turqoise
+ellipse(midX-25,midY-20,wing-15,wing-25);
+ellipse(midX-25,midY+10,wing-15,wing-25);
+ellipse(midX+25,midY-20,wing-15,wing-25);
+ellipse(midX+25,midY+10,wing-15,wing-25);
+
+fill(6,2,112);
+ellipse(midX-25,midY-20,wing-25,wing-35);
+ellipse(midX-25,midY+10,wing-25,wing-35);
+ellipse(midX+25,midY-20,wing-25,wing-35);
+ellipse(midX+25,midY+10,wing-25,wing-35);
+
+//body
+strokeWeight(1);
+stroke(6,2,112);
+fill(204,108,231);
+ellipse(midX,midY-5,20,100);
+
+//eyes
+strokeWeight(0);
+fill(0);
+ellipse(midX-3,midY-43,3,3);
+ellipse(midX+3,midY-43,3,3);
+ellipse(midX,midY-38,5,3);
+
+
+
 }
