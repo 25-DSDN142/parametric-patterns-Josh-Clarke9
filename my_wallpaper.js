09d4146cf43b58281720060242f1a8efe0de6199
Josh-Clarke9 <clarkejosh2@myvuw.ac.nz>
2025-07-29 11:40:52 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,8 +6,8 @@ let size = 40;
 let wing = 60;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
- // pWallpaper.output_mode(GRID_WALLPAPER);
+//  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -21,41 +21,29 @@ function setup_wallpaper(pWallpaper) {
 function wallpaper_background() {
 
   //background(204,108,231);//pink background
+    
+  background(0);
+
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-  let red = color(255,0,0);
+  // let red = color(255,0,0);
+  // let BGC;
   let blue = color(0,0,255);
   let pink = color(204,108,231);
   let turquoise = color(93,226,231);
   let purple = color(6,2,112);
   let black = color(0);
 
-  let BGC = blue
-
-
-  // // background(6, 2, 112); //blue background
-  
-  // if( BGC == blue){
-  //   ellipse(0,0,40)
-    
-  // }
-  
-  
   
   var e = 5//eye sizing
   var z = 60 //outer wing widths
   var x = 50 //outer wing height
   
-  if (  e>4 ){
-    BGC=pink
-    
-  }
-
-  background(BGC); //blue background
-
-strokeWeight(0);
+ 
+  
+strokeWeight(0);//surrounding circle
 fill(turquoise);
 ellipse(midX+0,midY+0,200,200);
 
